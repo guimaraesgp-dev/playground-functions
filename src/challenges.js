38aba1be7f37b1e38f2add260ce1cd5f0a5a1c91
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-04-19 03:18:52 -0300

    Ate o exercicio 5

--- src/challenges.js
+++ src/challenges.js
@@ -15,22 +15,22 @@ function calcArea(base, height) {
   }
 
 // Desafio 3
-function splitSentence() {
-  // seu código aqui
+function splitSentence(string) {
+  return string.split (" ");
 }
 
 // Desafio 4
-function concatName() {
-  // seu código aqui
-}
+function concatName(array) {
+  return `${array[array.length - 1]}, ${array[0]}`;
+} 
 
 // Desafio 5
-function footballPoints() {
-  // seu código aqui
-}
+function footballPoints(wins,ties) {
+  return (wins * 3 + ties * 1);
+};
 
 // Desafio 6
-function highestCount() {
+function highestCount(array) {
   // seu código aqui
 }
 
