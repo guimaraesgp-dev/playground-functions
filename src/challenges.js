d2572ecc8241b5917d03d10f87fc67481dd07367
Gabriel Guimarães <gabrielguimaraes.dev@gmail.com>
2022-04-19 13:11:28 -0300

    Feito ate exercicio 10

--- src/challenges.js
+++ src/challenges.js
@@ -31,31 +31,101 @@ function footballPoints(wins,ties) {
 
 // Desafio 6
 function highestCount(array) {
-  // seu código aqui
+  let countNumber = 0;
+  let highestNumber = array[0];
+  for (let i of array) {
+    if ((i < 0) || (i < highestNumber)) {
+      countNumber = 0;
+    }
+    highestNumber = i;
+    countNumber += 1;
+  }
+  return countNumber;
 }
 
 // Desafio 7
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(mouse, cat1, cat2) {
+  let distanciaCat1 = mouse - cat1;
+  let distanciaCat2 = mouse - cat2;
+  if (Math.abs(distanciaCat1) < Math.abs(distanciaCat2)) { 
+    return 'cat1';
+  } if (Math.abs(distanciaCat1) > Math.abs(distanciaCat2)) {
+    return 'cat2';
+  }
+  return 'os gatos trombam e o rato foge';
 }
 
 // Desafio 8
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(array) {
+  let result = [];
+  for (let i of array) {
+    if (i % 15 === 0) {
+      result.push('fizzBuzz');
+    } else if (i % 3 === 0) {
+      result.push('fizz');
+    } else if (i % 5 === 0) {
+      result.push('buzz');
+    } else {
+      result.push('bug!');
+    }
+  }
+  return result;
 }
 
 // Desafio 9
-function encode() {
-  // seu código aqui
+function encode(param1) {
+    let codigo = '';
+    for (let index in param1) {
+      if (param1[index] === 'a') {
+        codigo += 1;
+      } else if (param1[index] === 'e') {
+        codigo += 2;
+      } else if (param1[index] === 'i') {
+        codigo += 3;
+      } else if (param1[index] === 'o') {
+        codigo += 4;
+      } else if (param1[index] === 'u') {
+        codigo += 5;
+      } else {
+        codigo += param1[index];
+      }
+    }
+    return codigo;
 }
-function decode() {
-  // seu código aqui
+
+function decode(param2) {
+  let texto = '';
+  for (let index2 in param2) {
+    if (param2[index2] === '1') {
+      texto += 'a';
+    } else if (param2[index2] === '2') {
+      texto += 'e';
+    } else if (param2[index2] === '3') {
+      texto += 'i';
+    } else if (param2[index2] === '4') {
+      texto += 'o';
+    } else if (param2[index2] === '5') {
+      texto += 'u';
+    } else {
+      texto += param2[index2];
+    }
+  }
+  return texto;
 }
 
 // Desafio 10
-function techList() {
-  // seu código aqui
-}
+function techList(tech, name) {
+   let theArray = [];
+  let theTech = tech.sort();
+  if (tech.length === 0) {
+    theArray = 'Vazio!';
+  } else {
+    for (let i = 0; i <= theTech.length - 1; i += 1) {
+      theArray.push({ tech: theTech[i], name: name });
+    }
+  }
+  return theArray;
+} 
 
 module.exports = {
   calcArea,
